d567d8fad503434ad3bc23edb924692ba9e20c2b
Yosef <yosefgrant@gmail.com>
2021-03-11 12:38:25 +0000

    comments added

--- clock.js
+++ clock.js
@@ -1,7 +1,11 @@
+// Assigning Variables from DOM
+
 let secondsProgress = document.getElementById('seconds-progress');
 let minutesProgress = document.getElementById('minutes-progress');
 let hoursProgress = document.getElementById('hours-progress');
 
+// Main function
+// ***START *** //
 function getTime() {
   const fullDate = new Date();
   let hours = fullDate.getHours();
@@ -12,6 +16,8 @@ function getTime() {
   let year = fullDate.getFullYear();
   let day = fullDate.getDay();
 
+  // 0 added to single digits to give digital aesthetic
+
   hours < 10 ? (hours = '0' + hours) : hours;
   mins < 10 ? (mins = '0' + mins) : mins;
   secs < 10 ? (secs = '0' + secs) : secs;
@@ -20,6 +26,7 @@ function getTime() {
   document.getElementById('minutes').innerHTML = `${mins}`;
   document.getElementById('seconds').innerHTML = `${secs}`;
 
+  /* DAYS */
   switch (day) {
     case 0:
       day = 'Sunday';
@@ -42,6 +49,8 @@ function getTime() {
     case 6:
       day = 'Saturday';
   }
+
+  /* MONTHS */
 
   switch (month) {
     case 0:
@@ -80,15 +89,20 @@ function getTime() {
       month = 'December';
   }
 
+  // Date suffixes - st, nd, rd, th
+
   let dateSuffix;
 
-  if ((date % 10 === 1) && (date !== 11)) {
+  if (date % 10 === 1 && date !== 11) {
+    // need to single out 11-13 as they use 'th' and break the rule
     dateSuffix = 'st';
-  } else if ((date % 10 === 2) && (date !== 12)) {
+  } else if (date % 10 === 2 && date !== 12) {
     dateSuffix = 'nd';
-  } else if ((date % 10 === 3) && (date !== 13)) {
+  } else if (date % 10 === 3 && date !== 13) {
     dateSuffix = 'rd';
   } else dateSuffix = 'th';
+
+  // Setting up the DOM
 
   document.getElementById('day').innerHTML = `${day}`;
   document.getElementById(
@@ -96,13 +110,14 @@ function getTime() {
   ).innerHTML = `the ${date}${dateSuffix} of ${month}`;
   document.getElementById('year').innerHTML = `${year}`;
 
-  secondsProgress.style.setProperty('--percentage', `${secs * 6.1}deg`);
+  secondsProgress.style.setProperty('--percentage', `${secs * 6.1}deg`); // 6.1 to ensure a full circle before restarting
   minutesProgress.style.setProperty('--percentage', `${mins * 6.1}deg`);
   hoursProgress.style.setProperty('--percentage', `${hours * 15.1}deg`);
 
   let timeOfDay = document.getElementById('time-of-day');
 
   if (hours >= 6 && hours < 19) {
+    // time of day arbitary. in future would be useful to have this based on a sunrise/set cycle
     timeOfDay.innerHTML = `
         <img src='./assets/day.png' alt='daytime'/>
         `;
@@ -112,9 +127,10 @@ function getTime() {
         `;
   }
 }
+// *** END *** //
 
+// Callback function; interval set to update every second
 setInterval(getTime, 100);
 
 // UPDATES
-
 // add international + city times
